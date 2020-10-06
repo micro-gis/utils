0e9ca98bb7186f400f7b263f704adf07cb4bcc8c
Yossef <Azoulay>
2020-10-06 18:45:37 +0200

    refactor error lib

--- rest_errors/go.mod
+++ rest_errors/go.mod
@@ -1,3 +1,5 @@
 module github.com/micro-gis/utils/rest_errors
 
 go 1.14
+
+require github.com/stretchr/testify v1.6.1
--- rest_errors/rest_test.go
+++ rest_errors/rest_test.go
@@ -8,18 +8,18 @@ import (
 )
 
 func TestNewInternalServerError(t *testing.T) {
-	err:= NewInternalServerError("any message", errors.New("database error"))
+	err := NewInternalServerError("any message", errors.New("database error"))
 	assert.NotNil(t, err)
-	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
-	assert.EqualValues(t, "any message", err.Message)
-	assert.EqualValues(t, "Internal_server_error", err.Err)
-	assert.NotNil(t, err.Causes)
-	assert.EqualValues(t, 1, len(err.Causes) )
-	assert.EqualValues(t, "database error", err.Causes[0])
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status())
+	assert.EqualValues(t, "any message", err.Message())
+	assert.EqualValues(t, "Internal_server_error", err.Err())
+	assert.NotNil(t, err.Causes())
+	assert.EqualValues(t, 1, len(err.Causes()))
+	assert.EqualValues(t, "database error", err.Causes()[0])
 }
 
 func TestNewBadRequestError(t *testing.T) {
-//TODO : implement
+	//TODO : implement
 }
 
 func TestNewNotFoundError(t *testing.T) {
